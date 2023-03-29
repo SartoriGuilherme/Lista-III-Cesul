204c8b3e7040acfdb88b38ac6b53a5af7c094537
SartoriGuilherme <128654718+SartoriGuilherme@users.noreply.github.com>
2023-03-29 20:29:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+area = float(input('Informe a área da casa: '))
+
+rejunte = (area / 3)
+
+argamassa = (area / 5)
+
+print("Serão necessários", rejunte, "kg de rejunte." )
+
+print("Serão necessários", argamassa, "kg de argamassa.")
