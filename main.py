1e4008f31fda1c40175df48c8b9044ad0894f37a
SartoriGuilherme <128654718+SartoriGuilherme@users.noreply.github.com>
2023-03-29 21:48:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+area = float(input('Informe a área da casa: '))
+
+rejunte = (area / 3)
+argamassa = (area / 5)
+
+print(f'A quantidade de rejunte necessário é {rejunte:.2f}Kg.')
+print(f'A quantidade de argamassa necessário é {argamassa:.2f}Kg.')
