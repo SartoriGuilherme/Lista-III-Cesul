a08256d973bd82383b09c1411235043a28025e81
SartoriGuilherme <128654718+SartoriGuilherme@users.noreply.github.com>
2023-03-29 21:48:11 -0300

    Delete main5.py

--- main5.py
+++
@@ -1,9 +0,0 @@
-area = float(input('Informe a área da casa: '))
-
-rejunte = (area / 3)
-
-argamassa = (area / 5)
-
-print("Serão necessários", rejunte, "kg de rejunte." )
-
-print("Serão necessários", argamassa, "kg de argamassa.")
